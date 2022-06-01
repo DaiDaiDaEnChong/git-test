e85841b422e641ca52993fd3c62e9e6175080409
ysp <392768922@qq.com>
2022-06-01 20:37:46 +0800

    hot-fix22 commit

--- src/main/java/com/pdd/Test.java
+++ src/main/java/com/pdd/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("niudeyi");
         System.out.println("niude2");
+        System.out.println("niude3");
     }
 }
